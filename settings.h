64499b21ba473ad8463ba0d3aa7427b26dce551d
Julian <855526+julyworlds@users.noreply.github.com>
2022-09-20 22:14:48 +0200

    Create settings.h

---
+++ settings.h
@@ -0,0 +1,6 @@
+String wifi_essid = "WLAN";
+String wifi_password = "passwordd";
+String mqtt_server = "192.168.0.1";
+int mqtt_port = 1883;
+String mqtt_user = "user";
+String mqtt_password = "passw";
